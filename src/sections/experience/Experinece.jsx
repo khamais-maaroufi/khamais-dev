949bcbedff272d4fb1644f030b17ce0255b9a431
khamais-maaroufi <khamais.sil@gmail.com>
2023-11-13 18:58:01 +0100

    finished-experience-section

--- src/sections/experience/Experinece.jsx
+++ src/sections/experience/Experinece.jsx
@@ -7,8 +7,11 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
-import fenix from "../../assets/Fg.png";
-
+import fenix from "../../assets/Fenix-Future-Dev.jpg";
+import hg from "../../assets/Hg.png";
+import Mg from "../../assets/Mg.png";
+import Ng from "../../assets/Ng.png";
+import experienceData from "./experineceData";
 const Experinece = () => {
   const { language } = useContext(LanguageContext);
   return (
@@ -24,7 +27,77 @@ const Experinece = () => {
             color: "var(--primary)",
           }}
           contentArrowStyle={{ borderRight: "7px solid  var(--primary)" }}
-          date="2011 - present"
+          date={language == "English" ? "June 2022" : "Juin 2022"}
+          iconStyle={{
+            background: "var(--accent)",
+            boxShadow: "none",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          icon={
+            <div className="logo-timeline-conatiner">
+              <img
+                src={Mg}
+                alt="université-tunis-manar"
+                className="logo-timeline"
+              />
+            </div>
+          }
+        >
+          <h3 className="vertical-timeline-element-title">
+            {experienceData.university.title[language]}
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            {experienceData.university.location[language]}
+          </h4>
+          <p>{experienceData.university.description[language]}</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{
+            background: "var(--glass-bg)",
+            color: "var(--primary)",
+          }}
+          contentArrowStyle={{ borderRight: "7px solid  var(--primary)" }}
+          date={
+            language == "English"
+              ? "February 2022 - June 2022"
+              : "Février 2022 - Juin 2022"
+          }
+          iconStyle={{
+            background: "var(--accent)",
+            boxShadow: "none",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          icon={
+            <div className="logo-timeline-conatiner">
+              <img src={hg} alt="horizondata" className="logo-timeline" />
+            </div>
+          }
+        >
+          <h3 className="vertical-timeline-element-title">
+            {experienceData.horizondata.title[language]}
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            {experienceData.horizondata.location[language]}
+          </h4>
+          <p>{experienceData.horizondata.description[language]}</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{
+            background: "var(--glass-bg)",
+            color: "var(--primary)",
+          }}
+          contentArrowStyle={{ borderRight: "7px solid  var(--primary)" }}
+          date={
+            language === "English"
+              ? "August 2022 - October 2022"
+              : "Août 2022 - Octobre 2022"
+          }
           iconStyle={{
             background: "var(--accent)",
             boxShadow: "none",
@@ -38,12 +111,46 @@ const Experinece = () => {
             </div>
           }
         >
-          <h3 className="vertical-timeline-element-title">Creative Director</h3>
-          <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-          <p>
-            Creative Direction, User Experience, Visual Design, Project
-            Management, Team Leading
-          </p>
+          <h3 className="vertical-timeline-element-title">
+            {experienceData.fenix.title[language]}
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            {experienceData.fenix.location[language]}
+          </h4>
+          <p>{experienceData.fenix.description[language]}</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{
+            background: "var(--glass-bg)",
+            color: "var(--primary)",
+          }}
+          contentArrowStyle={{ borderRight: "7px solid  var(--primary)" }}
+          date={
+            language == "English"
+              ? "February 2023 - May 2023"
+              : "Février 2023 - Mai 2023"
+          }
+          iconStyle={{
+            background: "var(--accent)",
+            boxShadow: "none",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          icon={
+            <div className="logo-timeline-conatiner">
+              <img src={Ng} alt="next-consult" className="logo-timeline" />
+            </div>
+          }
+        >
+          <h3 className="vertical-timeline-element-title">
+            {experienceData.next.title[language]}
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            {experienceData.next.location[language]}
+          </h4>
+          <p>{experienceData.next.description[language]}</p>
         </VerticalTimelineElement>
       </VerticalTimeline>
     </section>
--- src/sections/experience/experineceData.js
+++ src/sections/experience/experineceData.js
@@ -1,72 +1,66 @@
 const experienceData = {
   university: {
     title: {
-      English: "I obtained my License degree in computer science",
-      Français: "",
+      English: "I obtained my bachelor's degree in computer science",
+      Français: "J'ai obtenu ma licence en informatique",
     },
     location: {
-      English: "",
-      Français: "",
+      English: "Tunis El Manar University",
+      Français: "Université de Tunis El Manar",
     },
     description: {
-      English: "",
-      Français: "",
-    },
-  },
-  manar: {
-    title: {
-      English: "Graduated with a License (Bachelor) degree",
-      Français: "",
-    },
-    location: {
-      English: "",
-      Français: "",
-    },
-    description: {
-      English: "",
-      Français: "",
+      English:
+        "Acquired fundamental knowledge in computer science through coursework and practical projects.",
+      Français:
+        "Acquis des connaissances fondamentales en informatique grâce à des cours et des projets pratiques.",
     },
   },
   horizondata: {
     title: {
-      English: "Graduated with a License (Bachelor) degree",
-      Français: "",
+      English: "End of study Internship",
+      Français: "Stage de fin d'études",
     },
     location: {
-      English: "",
-      Français: "",
+      English: "Horizon Data, Tunis",
+      Français: "Horizon Data, Tunis",
     },
     description: {
-      English: "",
-      Français: "",
+      English:
+        "I worked on the development of a web application for an Education Management System (LMS).",
+      Français:
+        "J'ai travaillé sur le développement d'une application web pour un système de gestion de l'éducation(LMS).",
     },
   },
   fenix: {
     title: {
-      English: "Graduated with a License (Bachelor) degree",
-      Français: "",
+      English: "Volunteer internship in web development",
+      Français: "Stage Bénévolat en développement web",
     },
     location: {
-      English: "",
-      Français: "",
+      English: "Fenix Future Dev (Remote)",
+      Français: "Fenix Future Dev (à distance)",
     },
     description: {
-      English: "",
-      Français: "",
+      English:
+        "I worked on the development of a web application (SaaS) for a startup.",
+      Français:
+        "J'ai travaillé sur le développement d'une application web (SAAS) pour une start-up.",
     },
   },
   next: {
     title: {
-      English: "Graduated with a License (Bachelor) degree",
-      Français: "",
+      English: "Started working as a web developer",
+      Français: "J'ai commencé à travailler en tant que développeur web",
     },
     location: {
-      English: "",
-      Français: "",
+      English: "Next consult, Tunis",
+      Français: "Next consult, Tunis",
     },
     description: {
-      English: "",
-      Français: "",
+      English:
+        "I worked on the development, deployment, and maintenance of software systems, most of which were ERP, CRM, and SaaS applications.",
+      Français:
+        "J'ai travaillé sur le développement, le déploiement et la maintenance de systèmes logiciels, dont la plupart étaient des logiciels ERP, CRM et SAAS.",
     },
   },
 };
